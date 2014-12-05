66817401d0affac254c4bd87320813325a37e57e
niquola <niquola@gmail.com>
2014-12-05 22:21:11 +0400

    add extract text

--- jsonb_extra.c
+++ jsonb_extra.c
@@ -19,17 +19,30 @@ typedef struct JbqResult
   int count;
 } JbqResult;
 
+typedef struct JbvResult
+{
+  JsonbValue **result;
+  int  size;
+  int count;
+} JbvResult;
+
 char *JsbvToStr(JsonbValue *v);
-char *JbToStr(Jsonb *v);
+/* char *JbToStr(Jsonb *v); */
 void debugJsonb(JsonbValue *jb);
-void walk(JbqResult *result, JsonbValue *jb, Datum *path, int level, int size);
+void walk(JbvResult *result, JsonbValue *jb, Datum *path, int level, int size);
 bool isArray(JsonbValue *v);
-void addJbqResult(JbqResult *result, JsonbValue *val);
+JsonbValue *JsonbToJsonbValue(Jsonb *v);
+/* void addJbqResult(JbqResult *result, JsonbValue *val); */
+void addToJbvResult(JbvResult *result, JsonbValue *val);
+JbvResult *jsonb_extract_internal(Jsonb *jb, ArrayType *path);
+text *jbvToText(JsonbValue *v);
 
 JsonbValue toJsonbString(Datum str);
 /* static void recursiveAny(JsonbValue *jb); */
 
 PG_FUNCTION_INFO_V1(jsonb_extract);
+PG_FUNCTION_INFO_V1(jsonb_extract_text);
+PG_FUNCTION_INFO_V1(jsonb_as_text);
 
 char *JsbvToStr(JsonbValue *v){
   Jsonb	*j;
@@ -37,18 +50,12 @@ char *JsbvToStr(JsonbValue *v){
   return JsonbToCString(NULL, &j->root, VARSIZE(j));
 }
 
-char *JbToStr(Jsonb *v){
-  return JsonbToCString(NULL, &v->root, VARSIZE(v));
-}
-
-void addJbqResult(JbqResult *result, JsonbValue *val){
+void addToJbvResult(JbvResult *result, JsonbValue *val){
   if (result->count >= result->size){
     result->size *= 2;
-    result->result = repalloc(result->result, sizeof(Jsonb *) * result->size);
+    result->result = repalloc(result->result, sizeof(JsonbValue *) * result->size);
   }
-  /* elog(DEBUG1, "ADDDDD: %s", JsbvToStr(val)); */
-  /* elog(DEBUG1, "ADDDDD: %s", JbToStr(JsonbValueToJsonb(val))); */
-  result->result[result->count++] = JsonbValueToJsonb(val);
+  result->result[result->count++] = val;
 }
 
 bool isArray(JsonbValue *v){
@@ -61,23 +68,95 @@ bool isArray(JsonbValue *v){
     return false;
   }
 }
+JsonbValue *
+JsonbToJsonbValue(Jsonb *v){
+  JsonbIterator *it = JsonbIteratorInit((JsonbContainer *) &v->root);
+  return NULL;
+}
 
-  Datum
+Datum
+jsonb_as_text(PG_FUNCTION_ARGS)
+{
+  Jsonb *jb = PG_GETARG_JSONB(0);
+  JsonbValue *jv = JsonbToJsonbValue(jb);
+  PG_RETURN_NULL();
+}
+
+Datum
 jsonb_extract(PG_FUNCTION_ARGS)
 {
   Jsonb *jb = PG_GETARG_JSONB(0);
   ArrayType  *path = PG_GETARG_ARRAYTYPE_P(1);
 
+  JbvResult *result;
+  Jsonb **jsonbs;
+  ArrayType  *result_array;
+  int i;
+
+  result = jsonb_extract_internal(jb, path);
+  if (result->count == 0) {
+    PG_RETURN_NULL();
+  }else{
+    jsonbs = palloc(result->count * sizeof(Jsonb *));
+    for(i=0; i< result->count; i++){
+      /* elog(INFO, "%s", JsbvToStr(result->result[i])); */
+      jsonbs[i] = JsonbValueToJsonb(result->result[i]);
+    }
+    result_array = construct_array((Datum *) jsonbs, result->count, JSONBOID, -1, false, 'i');
+
+    for(i=0; i< result->count; i++)
+      pfree(jsonbs[i]);
+
+    pfree(jsonbs);
+    PG_RETURN_POINTER(result_array);
+  }
+
+}
+
+Datum
+jsonb_extract_text(PG_FUNCTION_ARGS)
+{
+  Jsonb *jb = PG_GETARG_JSONB(0);
+  ArrayType  *path = PG_GETARG_ARRAYTYPE_P(1);
+
+  JbvResult  *result;
+  text **texts;
+  ArrayType *result_texts;
+
+  int i;
+  /* ArrayType  *texts; */
+
+  result = jsonb_extract_internal(jb, path);
+  if (result->count == 0) {
+    PG_RETURN_NULL();
+  }else{
+    texts = palloc(result->count * sizeof(text));
+    for(i=0; i< result->count; i++){
+
+      /* elog(INFO, "%s", JsbvToStr(result->result[i])); */
+      texts[i] = jbvToText(result->result[i]);
+    }
+    result_texts = construct_array((Datum *) texts, result->count, TEXTOID, -1, false, 'i');
+    for(i=0; i< result->count; i++)
+      pfree(texts[i]);
+
+    pfree(texts);
+    PG_RETURN_POINTER(result_texts);
+  }
+}
+
+
+JbvResult
+*jsonb_extract_internal(Jsonb *jb, ArrayType *path)
+{
   Datum	  *pathtext;
   bool	  *pathnulls;
   int 		npath;
-  int i;
 
   JsonbValue	jbv;
-  JbqResult *result;
-  ArrayType  *result_array;
+  JbvResult *result;
 
-  result = palloc(sizeof(JbqResult));
+  result = palloc(sizeof(JbvResult));
   result->size = 256;
   result->count = 0;
   result->result = palloc(256 * sizeof(JsonbValue *));
@@ -91,25 +170,38 @@ jsonb_extract(PG_FUNCTION_ARGS)
 
   walk(result, &jbv, pathtext, 0, npath);
 
-  elog(DEBUG1, "Result count: %i", result->count);
-
-  if(result->count > 0){
-    result_array = construct_array((Datum *) result->result, result->count, JSONBOID, -1, false, 'i');
-
-    for (i = 0; i < result->count; i++){
-      elog(DEBUG1, "* %s", JbToStr(result->result[i]));
-      pfree(result->result[i]);
-    }
-    pfree(result->result);
-    pfree(result);
-  }
-
-  if(result->count > 0){
-    PG_RETURN_POINTER(result_array);
-  }else{
-    PG_RETURN_NULL();
-  }
+  /* elog(DEBUG1, "Result count: %i", result->count); */
+  return result;
 }
+
+text *jbvToText(JsonbValue *v){
+  switch(v->type)
+  {
+    case jbvNull:
+      break;
+    case jbvBool:
+      return cstring_to_text(v->val.boolean ? "true" : "false");
+      break;
+    case jbvString:
+      return cstring_to_text_with_len(v->val.string.val, v->val.string.len);
+      break;
+    case jbvNumeric:
+      return cstring_to_text(DatumGetCString(DirectFunctionCall1(numeric_out,
+              PointerGetDatum(v->val.numeric))));
+      break;
+    case jbvBinary:
+      {
+        StringInfo  jtext = makeStringInfo();
+        (void) JsonbToCString(jtext, v->val.binary.data, -1);
+        return cstring_to_text_with_len(jtext->data, jtext->len);
+      }
+      break;
+    default:
+      elog(ERROR, "Wrong jsonb type: %d", v->type);
+  }
+  return NULL;
+}
+
 
 JsonbValue toJsonbString(Datum str){
   JsonbValue	key;
@@ -119,7 +211,7 @@ JsonbValue toJsonbString(Datum str){
   return key;
 }
 
-void walk(JbqResult *result, JsonbValue *jb, Datum *path, int level, int size){
+void walk(JbvResult *result, JsonbValue *jb, Datum *path, int level, int size){
   JsonbValue	key;
   JsonbValue *next_value;
   JsonbIterator *array_it;
@@ -127,14 +219,14 @@ void walk(JbqResult *result, JsonbValue *jb, Datum *path, int level, int size){
   int next_it;
 
 
-  elog(DEBUG1,"%i %i", level, size);
+  /* elog(DEBUG1,"%i %i", level, size); */
   /* debugJsonb(jb); */
 
   if (level == size){
-    addJbqResult(result, jb);
+    addToJbvResult(result, jb);
     return;
   }else{
-    elog(DEBUG1, "take %s of %s", TextDatumGetCString(path[level]), JsbvToStr(jb));
+    /* elog(DEBUG1, "take %s of %s", TextDatumGetCString(path[level]), JsbvToStr(jb)); */
   }
 
   if(jb->type == jbvBinary){
@@ -175,52 +267,6 @@ void walk(JbqResult *result, JsonbValue *jb, Datum *path, int level, int size){
   }
 }
 
-
-/* Datum */
-/* nicola(PG_FUNCTION_ARGS) */
-/* { */
-/*   Jsonb *jb = PG_GETARG_JSONB(0); */
-/*   ArrayType  *path = PG_GETARG_ARRAYTYPE_P(1); */
-
-/*   Datum	  *pathtext; */
-/*   bool	  *pathnulls; */
-/*   int 		npath; */
-/*   int     i; */
-/*   JsonbValue *jbvp = NULL; */
-/*   JsonbValue	key; */
-/* 	JsonbContainer *container; */
-/*   int reslen = 0; */
-
-/*   deconstruct_array(path, TEXTOID, -1, false, 'i', &pathtext, &pathnulls, &npath); */
-/*   elog(NOTICE, "npath %i", npath); */
-
-/* 	container = &jb->root; */
-/*   for (i = 0; i < npath; i++) { */
-
-/*     key.type = jbvString; */
-/*     key.val.string.val = VARDATA_ANY(pathtext[i]); */
-/*     key.val.string.len = VARSIZE_ANY_EXHDR(pathtext[i]); */
-
-/*     jbvp = findJsonbValueFromContainer(container, JB_FOBJECT, &key); */
-/*     elog(NOTICE,"key %s: step %s, %i", */
-/*         TextDatumGetCString(pathtext[i]), */
-/*         JsbvToStr(jbvp), */
-/*         jbvp->type); */
-/*     if(jbvp->type == jbvBinary){ */
-/*       container = (JsonbContainer *) jbvp->val.binary.data; */
-/*     }else{ */
-/*       PG_RETURN_NULL(); */
-/*     } */
-/*   } */
-
-/*   if(jbvp == NULL){ */
-/*     PG_RETURN_NULL(); */
-/*   }else{ */
-/*     PG_RETURN_JSONB(JsonbValueToJsonb(jbvp)); */
-/*   } */
-/* } */
-
-
 void debugJsonb(JsonbValue *jb){
   switch (jb->type)
   {
@@ -249,87 +295,3 @@ void debugJsonb(JsonbValue *jb){
       elog(DEBUG1, "D:other");
   }
 }
-
-/* static void */
-/* old_recursiveAny(JsonbValue *jb) { */
-/*   JsonbIterator	*it; */
-/*   int32			 r; */
-/*   JsonbValue v; */
-
-/*   elog(DEBUG1, "ITERATE %s",JsbvToStr(jb)); */
-
-/*   it = JsonbIteratorInit(jb->val.binary.data); */
-/*   while((r = JsonbIteratorNext(&it, &v, false)) != WJB_DONE) */
-/*   { */
-/*     if (r == WJB_KEY) */
-/*     { */
-/*       elog(DEBUG1, "key: %s", JsbvToStr(&v)); */
-/*       r = JsonbIteratorNext(&it, &v, false); */
-/*       Assert(r == WJB_VALUE); */
-/*     } */
-/*     if (r == WJB_VALUE || r == WJB_ELEM) */
-/*     { */
-/*       elog(DEBUG1,"  value type: %i, %s", v.type, JsbvToStr(&v)); */
-/*       switch (v.type) */
-/*       { */
-/*         case jbvNull: */
-/*           elog(DEBUG1, "jbvNull"); */
-/*           break; */
-/*         case jbvString: */
-/*           elog(DEBUG1, "jbvString, %s", JsbvToStr(&v)); */
-/*           break; */
-/*         case jbvNumeric: */
-/*           elog(DEBUG1, "jbvNumeric, %s", JsbvToStr(&v)); */
-/*           break; */
-/*         case jbvBool: */
-/*           elog(DEBUG1, "jbvBool"); */
-/*           break; */
-/*         case jbvArray: */
-/*           elog(DEBUG1, "jbvArray"); */
-/*           /1* recursiveAny(&v); *1/ */
-/*           break; */
-/*         case jbvObject: */
-/*           elog(DEBUG1, "jbvObject"); */
-/*           /1* recursiveAny(&v); *1/ */
-/*           break; */
-/*         case jbvBinary: */
-/*           elog(DEBUG1, "jbvBinary"); */
-/*           /1* recursiveAny(&v); *1/ */
-/*           break; */
-/*         default: */
-/*           elog(DEBUG1, "other"); */
-/*       } */
-/*       /1* recursiveAny(&v); *1/ */
-/*       /1* if (v.type == jbvBinary) *1/ */
-/*       /1* 	recursiveAny(&v); *1/ */
-/*     } */
-/*   } */
-/* } */
-
-/* text  *key = PG_GETARG_TEXT_P(1); */
-/* JsonbValue	k; */
-/* uint32 keylen; */
-
-/* container = &jb->root; */
-
-/* keylen = VARSIZE_ANY_EXHDR(key); */
-/* k.type = jbvString; */
-/* k.val.string.val = text_to_cstring(key); */
-/* k.val.string.len = keylen; */
-
-/* elog(DEBUG1, "key is %s, %i", text_to_cstring(key), keylen); */
-
-/* jbvp = findJsonbValueFromContainer(container, */
-/* JB_FOBJECT, &k); */
-
-/* elog(DEBUG1, "Get value"); */
-
-/* if (jbvp == NULL) */
-/* PG_RETURN_NULL(); */
-
-/* res = JsonbValueToJsonb(jbvp); */
-
-/* if (res == NULL) */
-/* PG_RETURN_NULL(); */
-/* else */
-/* PG_RETURN_JSONB(res); */
--- sql/jsonb_extra.sql
+++ sql/jsonb_extra.sql
@@ -3,11 +3,15 @@
 DROP EXTENSION jsonb_extra;
 CREATE EXTENSION jsonb_extra;
 
-SELECT jsonb_extract('{"a":"1", "b":{"c":"2", "d": [1,2,{"x":100}], "e":{"f": 4}}}', '{b,e,f}');
-SELECT jsonb_extract('{"b":{"c":"2"}}', '{b,c}');
-SELECT jsonb_extract('{"b":[{"c":1},{"c":2},{"x":"ups"}]}', '{b,c}');
+/* SELECT jsonb_extract('{"a":"1", "b":{"c":"2", "d": [1,2,{"x":100}], "e":{"f": 4}}}', '{b,e,f}'); */
+/* SELECT jsonb_extract('{"b":{"c":"2"}}', '{b,c}'); */
+/* SELECT jsonb_extract('{"b":[{"c":1},{"c":2},{"x":"ups"}]}', '{b,c}'); */
 
 
-SELECT jsonb_extract('{"b":[{"c":{"x": 5}},{"c":2},{"x":"ups"}]}', '{b,c}');
+/* SELECT jsonb_extract('{"b":[{"c":{"x": 5}},{"c":2},{"x":"ups"}]}', '{b,c}'); */
+
+/* SELECT jsonb_extract_text('{"b":[{"c":{"x": 5}},{"c":2},{"x":"ups"}]}', '{b,c}'); */
+
+SELECT jsonb_as_text('"x"'::jsonb);
 
 --}}}
